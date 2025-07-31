77fad31d0c457dac0bc3f71981a73ae544fbd6de
Jeon Jihwan <jeonjihwan72@gmail.com>
2025-07-31 11:03:26 +0900

    introduction for differ of object and instance, reference

--- app/src/main/java/org/example/myapp/App.java
+++ app/src/main/java/org/example/myapp/App.java
@@ -1,7 +1,5 @@
 package org.example.myapp;
 
-import java.util.Scanner;
-
 /** 이 클래스는 한밭대학교 특강 실습 예제를 위한 첫 번재 클래스입니다. <code>App</code> 클래스! */
 public class App {
 
@@ -13,10 +11,31 @@ public class App {
   public static void main(String[] args) {
     //    Scanner scanner = new Scanner(new FileInputStream("a.txt"));
     //    Scanner scanner = new Scanner(new ByteArrayInputStream());
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("이름? ");
-    String name = scanner.nextLine();
-    System.out.printf("안녕하세요 %s 님!\n", name);
-    scanner.close();
+
+    //    Scanner scanner = new Scanner(System.in);
+    //    System.out.print("이름? ");
+    //    String name = scanner.nextLine();
+    //    System.out.printf("안녕하세요 %s 님!\n", name);
+    //    scanner.close();
+
+    class Calculator {
+      int result;
+
+      void plus(int a) {
+        this.result += a;
+      }
+
+      void minus(int a) {
+        this.result -= a;
+      }
+    }
+
+    Calculator c1 = new Calculator(); // 객체 주소를 담아둔 c1은 'reference'라고 한다.
+    Calculator c2 = new Calculator();
+
+    c1.plus(100); // 실무에서는 레퍼런스와 객체를 구분하지 않고 '객체'로 동일하게 칭한다.
+    c2.plus(555);
+
+    c1 = new Calculator();
   }
 }
